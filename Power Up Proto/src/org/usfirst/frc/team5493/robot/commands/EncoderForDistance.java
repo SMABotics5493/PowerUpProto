3237c166103bc58ea4b74e86d0a7d61b26201d3c
Ben <smabotics@bkeastmond.com>
2018-02-14 12:43:59 -0700

    Tweaking some settings

--- Power Up Proto/src/org/usfirst/frc/team5493/robot/commands/EncoderForDistance.java
+++ Power Up Proto/src/org/usfirst/frc/team5493/robot/commands/EncoderForDistance.java
@@ -30,7 +30,7 @@ public class EncoderForDistance extends Command {
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
-    	Robot.driveBase.tankDrive(speed, speed, 0, 0);
+//    	Robot.driveBase.tankDrive(speed, speed, 0, 0);
     }
 
     // Make this return true when this Command no longer needs to run execute()
